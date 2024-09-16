e41940b3848442f2b71ff3eaffe96b70dd69de49
Felipe José de Lima <lplplimald@gmail.com>
2024-09-15 21:53:05 -0300

    third commit

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      'styled-components': require.resolve('styled-components')
+      'styled-components': 'styled-components'
     }
   }
 });
